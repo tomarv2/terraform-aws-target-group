f862cb3b296252c7e2b043563dfdb446cd4c2a55
tomarv2 <varuntomar2019@gmail.com>
2021-02-16 22:56:37 -0800

    initial commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 module "global" {
-    source                    = "git::git@github.com:tomarv2/terraform-global.git"
+    source                    = "git::git@github.com:tomarv2/terraform-global.git//aws"
 }
 
 locals {
