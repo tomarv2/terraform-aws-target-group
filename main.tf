7f6005f335b868492acac1e1bfbc538e6ae86537
tomarv2 <varuntomar2019@gmail.com>
2021-02-16 23:36:55 -0800

    cleanup

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 module "global" {
-    source                    = "git::git@github.com:tomarv2/terraform-global.git//aws"
+    source                    = "git::git@github.com:tomarv2/terraform-global.git?ref=0.0.1//aws"
 }
 
 locals {
